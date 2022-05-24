06ced2c5e560b7298bc9dee52aa5aef34d8b3a6f
Olivier Prouvost <olivier.prouvost@opcoach.com>
2022-05-24 17:58:22 +0200

    Issue 1108 : text on perspective switcher on start

--- SAMNG_Framework/src/com/faiveley/samng/principal/ihm/SamAddon.java
+++ SAMNG_Framework/src/com/faiveley/samng/principal/ihm/SamAddon.java
@@ -4,15 +4,21 @@ package com.faiveley.samng.principal.ihm;
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
+import org.eclipse.ui.internal.util.PrefUtil;
 
 public class SamAddon {
 
 	
+	@SuppressWarnings("restriction")
 	@PostConstruct
 	public void applicationStarted(MApplication a)
 	{
 	   // Remove the minmax Addon (see https://www.eclipse.org/eclipse/news/4.15/platform_isv.php#disable-swt-addons)
 		a.getTags().add("DisableMinMaxAddon");  // private constant defined in MinMaxAddon class !
+		
+		// Set text on perspective
+		PrefUtil.getAPIPreferenceStore().setDefault(IWorkbenchPreferenceConstants.SHOW_TEXT_ON_PERSPECTIVE_BAR, true);
 	}
 
 }
